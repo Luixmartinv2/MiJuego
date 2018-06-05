461d3d659965f3a8dc730c35bfb1c5039a8cbccc
luixmartinv2 <luixmartinv2@gmail.com>
2018-06-05 11:15:59 +0200

    Commit 14

--- MiJuego/android/src/com/mijuego/AndroidLauncher.java
+++ MiJuego/android/src/com/mijuego/AndroidLauncher.java
@@ -4,20 +4,16 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mijuego.ConexionFirebase.LoginFragment;
-import com.mijuego.ConexionFirebase.RegisterFragment;
-import com.mijuego.MiJuego;
+
 
 public class AndroidLauncher extends AndroidApplication {
 
-	LoginFragment loginFragment;
-	RegisterFragment registerFragment;
+
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//mostramos el login
-		setContentView(R.layout.layout);
+
 
 
 
